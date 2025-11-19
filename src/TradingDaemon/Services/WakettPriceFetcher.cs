2bdcf35df9b59061a27155f8b675b28db556574a
Philippe Huber <phu@quantumqb.com>
2025-11-19 09:30:39 +0000

    dsfdsvdv

--- src/TradingDaemon/Services/WakettPriceFetcher.cs
+++ src/TradingDaemon/Services/WakettPriceFetcher.cs
@@ -290,9 +290,6 @@ public class WakettPriceFetcher
         var nowUtc = DateTimeOffset.UtcNow;
         var historicalWindowStart = nowUtc.Subtract(HistoricalWindow).UtcDateTime;
 
-        var nowUtc = DateTimeOffset.UtcNow;
-        var historicalWindowStart = nowUtc.Subtract(HistoricalWindow).UtcDateTime;
-
         var missingByOffset = new List<(int MinuteOffset, IReadOnlyList<DateTime> Missing)>();
         foreach (var minuteOffset in PriceMinuteOffsets)
         {
